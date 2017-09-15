ddfc6d0bf6f7edfd16f4336297749124360356ff
kazifarhan <kazifarhan199@gmail.com>
2017-09-15 11:26:56 -0700

    New

---
+++ if_else_if/if_else_if.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+	int a;
+
+	printf("Enter a command [1-3] : \n");
+	scanf("%d",&a);
+
+	if (a==1){
+	printf("You are the best !!, Believe it :) ");
+	}
+	else if (a==2){
+		printf("Believe in Your self :) ");
+	}
+	else{
+	printf("Juse Believe in you'r self :) ");
+	}
+	
+	getch();
+}
