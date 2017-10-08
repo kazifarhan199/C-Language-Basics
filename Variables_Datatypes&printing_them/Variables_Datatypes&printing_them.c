01059e57602a53caf5afcdebb47d6f62566e2151
kazifarhan <kazifarhan199@gmail.com>
2017-10-08 21:58:00 +0530

    updated

---
+++ Variables_Datatypes&printing_them/Variables_Datatypes&printing_them.c
@@ -0,0 +1,18 @@
+#include<conio.h>
+#include<stdio.h>
+
+int main()
+{
+    int a;
+    char b;
+    float c;
+
+    a=10;
+    b='p';
+    c=10.45784;
+
+    printf(" %d \n %c \n %f",a,b,c);
+
+}
+
+
