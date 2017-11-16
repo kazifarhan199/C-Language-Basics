c9693d461ed37cf15b681e54be694bd3332186d9
KaziFarhanuddin <kazifarhan199@gmail.com>
2017-11-16 06:48:51 +0530

    Update

--- data structures/struct.c
+++ data structures/struct.c
@@ -5,12 +5,17 @@ struct child
 {
     char name[30];
     int roll_no;
-};                      //or "s;" (herse structure variable is "s")[either declare hear or below in main function]
+};
+//or "s;" (herse structure variable is "s")[either declare (or create object) hear or below in main function]
 
 void main()
 {
-    struct child s ;     //or "s;" (herse structure variable is "s")[either declare hear or above]
+    struct child s ;
+    //or "s;" (herse structure variable is "s")[either declare hear or above]
     printf("Enter the name \n" );
+
+    //Hear the "." is knwon as "dot operator"
+
     scanf("%s",&s.name );
     printf("Enter the roll_no\n" );
     scanf("%d",&s.roll_no );
